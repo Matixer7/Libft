be0104c568c0251b66c257c2857af901dece97ba
Matixer7 <mateusznr7@gmail.com>
2025-09-23 12:07:13 +0200

    ft_strrchr done corrected ruturn value from ft_strchr if nothing is found

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/23 10:42:02 by marvin            #+#    #+#             */
-/*   Updated: 2025/09/23 10:42:02 by marvin           ###   ########.fr       */
+/*   Created: 2025/09/23 11:55:56 by marvin            #+#    #+#             */
+/*   Updated: 2025/09/23 11:55:56 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(char *str, unsigned char c)
+char	*ft_strrchr(char *str, unsigned char c)
 {
-	char *p;
+	int		i;
+	char	*p;
 
 	p = str;
-	while (*p != c && *p != '\0' && *p)
-		p++;
+	i = 0;
+	while (p[i] != '\0')
+		i++;
+	i--;
+	while (p[i] != c && i >= 0 && p[i])
+		i--;
+	p += i;
 	return (p);
 }
 
 // int main(void)
 // {
-// 	char *str = "Find me I'm hexre";
+// 	char *str = "Fxind me I'm hexre";
 // 	char c = 'x';
-// 	str = ft_strchr(str, c);
+// 	str = ft_strrchr(str, c);
 // 	return 0;
 // }
