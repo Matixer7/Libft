458e042f808b823067f0bfeb3b55be4e8027eb62
Matixer7 <mateusznr7@gmail.com>
2025-09-23 08:53:09 +0200

    ft_strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgumienn <mgumienn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/23 08:38:28 by marvin            #+#    #+#             */
+/*   Updated: 2025/09/23 08:38:28 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[j])
+		j++;
+	if (size <= sizeof(dest))
+		return (size + sizeof(src));
+	while (src[i] && j < size)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (sizeof(dest) + sizeof(src));
+}
