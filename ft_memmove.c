ca749ee691ff49294e5efff99c43ffdc9165cda8
Matixer7 <mateusznr7@gmail.com>
2025-09-26 21:12:34 +0200

    fix: handle null pointers in ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -17,11 +17,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
+	if (!dest && !src)
+		return (NULL);
 	d = dest;
 	s = (unsigned char*) src;
-	if (d == s || n == 0)
-		*d = *s;
-
 	if (d < s)
 	{
 		while (n--)
