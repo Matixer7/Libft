a96615d5878b4872d02757db052185dea84538e1
Matixer7 <mateusznr7@gmail.com>
2025-09-26 18:57:46 +0200

    small mistake

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -26,7 +26,7 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 		while (str[i + j] == to_find[j] && (i + j) < n)
 		{
 			if (to_find[j + 1] == '\0')
-				return ((char*) str[i]);
+				return ((char*) &str[i]);
 			j++;
 		}
 		j = 0;
