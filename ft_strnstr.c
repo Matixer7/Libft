fdd9414fcb963bf765ead9acd02b53dda07b30f4
Matixer7 <mateusznr7@gmail.com>
2025-09-26 18:56:40 +0200

    fix in strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -20,13 +20,13 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 	i = 0;
 	j = 0;
 	if (to_find[0] == '\0')
-		return (str);
+		return ((char*) str);
 	while (str[i] != '\0' && i < n)
 	{
 		while (str[i + j] == to_find[j] && (i + j) < n)
 		{
 			if (to_find[j + 1] == '\0')
-				return (&str[i]);
+				return ((char*) str[i]);
 			j++;
 		}
 		j = 0;
