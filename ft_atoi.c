b5b523c967ca1897da3d78b12918cc3e51a59626
Mateusz Gumienny <mgumienn@c3r14s1.42warsaw.pl>
2025-09-28 14:08:12 +0200

    fix: refactor ft_atoi to handle sign directly and remove odd_or_even function

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,44 +6,33 @@
 /*   By: mgumienn <mgumienn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 15:58:35 by mgumienn          #+#    #+#             */
-/*   Updated: 2025/09/24 19:32:46 by mgumienn         ###   ########.fr       */
+/*   Updated: 2025/09/28 14:01:13 by mgumienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	odd_or_even(const char *str)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (str[i] && (str[i] == '+' || str[i] == '-'))
-	{
-		if (str[i] == '-')
-			j++;
-		i++;
-	}
-	return (j % 2);
-}
-
 int	ft_atoi(const char *str)
 {
 	int	i;
 	int	n;
+	int sign;
 
 	i = 0;
 	n = 0;
-	while (str[i] == '-' || str[i] == '+'
-		|| (str[i] <= '\r' && str[i] >= '\t') || str[i] == ' ')
+	sign = 1;
+	while ((str[i] <= '\r' && str[i] >= '\t') || str[i] == ' ')
 		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		n = n * 10 + (str[i] - 48);
 		i++;
 	}
-	if (odd_or_even(str) == 1)
-		n = -n;
-	return (n);
+	return (n * sign);
 }
