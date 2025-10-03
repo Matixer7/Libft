e231f7df805e92783b1bd1aeaa170df258813b42
Mateusz Gumienny <mateusznr7@gmail.com>
2025-10-03 18:01:12 +0200

    feat: added linked list functions ft_lstadd_front, ft_lstnew, and ft_lstsize; updated libft.h and Makefile

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgumienn <mgumienn@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 17:22:29 by mgumienn          #+#    #+#             */
+/*   Updated: 2025/10/03 17:40:10 by mgumienn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mgumienn <mgumienn@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 19:13:55 by mgumienn          #+#    #+#             */
-/*   Updated: 2025/10/02 18:36:29 by mgumienn         ###   ########.fr       */
+/*   Updated: 2025/10/03 17:58:34 by mgumienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include <stdint.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+
 int				ft_atoi(const char *str);
 void			*ft_bzero(void *ptr, size_t n);
 void			*ft_calloc(size_t count, size_t size);
@@ -26,6 +33,9 @@ int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
 char        	*ft_itoa(int n);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+t_list			*ft_lstnew(void *content);
+int				ft_lstsize(t_list *lst);
 void			*ft_memchr(const void *src, int search, size_t n);
 int				ft_memcmp(const void *s1, void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
